359d1229b0953e0ebaae19361038f420db0853d4
Andres Montenegro <1997jamm@gmail.com>
2024-11-17 22:33:50 -0500

    Se modifica el proyecto ciclos en javascript y html

--- Ciclos/js/ciclos.js
+++ Ciclos/js/ciclos.js
@@ -1,70 +1,68 @@
-// Función para crear y mostrar la tabla de multiplicar usando el ciclo 'for'
+// ciclo 'for'
 function mostrarTablaFor() {
-    let contenido = '<h2>Tabla de multiplicar con "for"</h2><ul>'; // Comienza el contenido HTML para la tabla con un título y una lista desordenada (ul)
-    for (let i = 1; i <= 10; i++) { // Ciclo 'for' que itera desde 1 hasta 10
-        contenido += `<li>6 x ${i} = ${6 * i}</li>`; // Genera una lista de multiplicaciones, por ejemplo: 5 x 1 = 5
+    let contenido = '<h2>Tabla de multiplicar con "for"</h2><ul>';
+    for (let i = 1; i <= 10; i++) { 
+        contenido += `<li>6 x ${i} = ${6 * i}</li>`; 
     }
-    contenido += '</ul>'; // Cierra la lista desordenada
-    document.getElementById('tabla-for').innerHTML = contenido; // Inserta el contenido generado en el div con id 'tabla-for'
+    contenido += '</ul>';
+    document.getElementById('tabla-for').innerHTML = contenido;
 }
 
-// Función para crear y mostrar la tabla de multiplicar usando el ciclo 'for in'
+// ciclo 'for in'
 function mostrarTablaForIn() {
-    let contenido = '<h2>Tabla de multiplicar con "for in"</h2><ul>'; // Título y lista
-    let tabla = {}; // Creamos un objeto vacío para almacenar las multiplicaciones
-    for (let i = 1; i <= 10; i++) { // Ciclo 'for' que llena el objeto con las multiplicaciones
-        tabla[i] = 7 * i; // Almacena cada multiplicación en el objeto con la clave 'i'
+    let contenido = '<h2>Tabla de multiplicar con "for in"</h2><ul>'; 
+    let tabla = {};
+    for (let i = 1; i <= 10; i++) { 
+        tabla[i] = 7 * i; 
     }
-    for (let i in tabla) { // Usamos el ciclo 'for in' para recorrer las propiedades del objeto 'tabla'
-        contenido += `<li>7 x ${i} = ${tabla[i]}</li>`; // Agrega cada multiplicación a la lista
+    for (let i in tabla) { 
+        contenido += `<li>7 x ${i} = ${tabla[i]}</li>`; 
     }
-    contenido += '</ul>'; // Cierra la lista desordenada
-    document.getElementById('tabla-for-in').innerHTML = contenido; // Inserta el contenido en el div con id 'tabla-for-in'
+    contenido += '</ul>'; 
+    document.getElementById('tabla-for-in').innerHTML = contenido;
 }
 
-// Función para crear y mostrar la tabla de multiplicar usando el ciclo 'for of'
+// ciclo 'for of'
 function mostrarTablaForOf() {
-    let contenido = '<h2>Tabla de multiplicar con "for of"</h2><ul>'; // Título y lista
-    let tabla = []; // Creamos un arreglo vacío
-    for (let i = 1; i <= 10; i++) { // Ciclo 'for' que llena el arreglo con las multiplicaciones
-        tabla.push(5 * i); // Empuja (agrega) cada resultado de la multiplicación al arreglo
+    let contenido = '<h2>Tabla de multiplicar con "for of"</h2><ul>'; 
+    let tabla = [];
+    for (let i = 1; i <= 10; i++) {
+        tabla.push(5 * i); 
     }
-    for (let valor of tabla) { // Ciclo 'for of' que recorre los valores del arreglo
-        contenido += `<li>5 x ${tabla.indexOf(valor) + 1} = ${valor}</li>`; // Agrega cada multiplicación a la lista
+    for (let valor of tabla) {
+        contenido += `<li>5 x ${tabla.indexOf(valor) + 1} = ${valor}</li>`; 
     }
-    contenido += '</ul>'; // Cierra la lista desordenada
-    document.getElementById('tabla-for-of').innerHTML = contenido; // Inserta el contenido en el div con id 'tabla-for-of'
+    contenido += '</ul>';
+    document.getElementById('tabla-for-of').innerHTML = contenido; 
 }
-
-// Función para crear y mostrar la tabla de multiplicar usando el ciclo 'while'
+//ciclo while
 function mostrarTablaWhile() {
-    let contenido = '<h2>Tabla de multiplicar con "while"</h2><ul>'; // Título y lista
-    let i = 1; // Inicia la variable 'i' en 1
-    while (i <= 10) { // Ciclo 'while' que se ejecuta mientras 'i' sea menor o igual a 10
-        contenido += `<li>9 x ${i} = ${9 * i}</li>`; // Agrega la multiplicación a la lista
-        i++; // Incrementa 'i' en 1
+    let contenido = '<h2>Tabla de multiplicar con "while"</h2><ul>'; 
+    let i = 1; 
+    while (i <= 10) { 
+        contenido += `<li>9 x ${i} = ${9 * i}</li>`; 
+        i++;
     }
-    contenido += '</ul>'; // Cierra la lista desordenada
-    document.getElementById('tabla-while').innerHTML = contenido; // Inserta el contenido en el div con id 'tabla-while'
+    contenido += '</ul>'; 
+    document.getElementById('tabla-while').innerHTML = contenido;
 }
-
-// Función para crear y mostrar la tabla de multiplicar usando el ciclo 'do while'
+//ciclo do while
 function mostrarTablaDoWhile() {
-    let contenido = '<h2>Tabla de multiplicar con "do while"</h2><ul>'; // Título y lista
-    let i = 1; // Inicia la variable 'i' en 1
+    let contenido = '<h2>Tabla de multiplicar con "do while"</h2><ul>';
+    let i = 1; 
     do {
-        contenido += `<li>10 x ${i} = ${10 * i}</li>`; // Agrega la multiplicación a la lista
-        i++; // Incrementa 'i' en 1
-    } while (i <= 10); // El ciclo 'do while' ejecuta el bloque al menos una vez, luego verifica si 'i' es menor o igual a 10
-    contenido += '</ul>'; // Cierra la lista desordenada
-    document.getElementById('tabla-do-while').innerHTML = contenido; // Inserta el contenido en el div con id 'tabla-do-while'
+        contenido += `<li>10 x ${i} = ${10 * i}</li>`; 
+        i++; 
+    } while (i <= 10); 
+    contenido += '</ul>'; 
+    document.getElementById('tabla-do-while').innerHTML = contenido; 
 }
 
-// Cuando el contenido de la página esté completamente cargado, ejecutamos las funciones para mostrar las tablas
+
 document.addEventListener("DOMContentLoaded", function() {
-    mostrarTablaFor(); // Llama a la función que muestra la tabla con 'for'
-    mostrarTablaForIn(); // Llama a la función que muestra la tabla con 'for in'
-    mostrarTablaForOf(); // Llama a la función que muestra la tabla con 'for of'
-    mostrarTablaWhile(); // Llama a la función que muestra la tabla con 'while'
-    mostrarTablaDoWhile(); // Llama a la función que muestra la tabla con 'do while
+    mostrarTablaFor(); 
+    mostrarTablaForIn(); 
+    mostrarTablaForOf(); 
+    mostrarTablaWhile(); 
+    mostrarTablaDoWhile(); 
 });
